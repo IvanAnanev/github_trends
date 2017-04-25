0ee21c4ef22f0724de62ad92a0e55842dbf743ef
Ivan Ananiev <ivcheg@gmail.com>
2017-04-25 14:23:39 +0300

    install amnesia elixir wrapper for mnesia

--- mix.exs
+++ mix.exs
@@ -28,12 +28,15 @@ defmodule GithubTrends.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
-     {:tentacat, "~> 0.5"}]
+    [
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+      {:tentacat, "~> 0.5"},
+      {:amnesia, github: "meh/amnesia", tag: :master}
+    ]
   end
 end
